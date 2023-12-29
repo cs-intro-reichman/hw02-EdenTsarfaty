1d63f8c3d283b82b20ab6d71949502428a58ab56
Eden Tsarfaty <eden@post.runi.ac.il>
2023-12-29 14:34:05 +0200

    DamkaBoard fix

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,14 +5,14 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int size = Integer.parseInt(args[0]);
 		String row = "";
-		for (int i = 0; i < size; i++)
+		for (int i = 1; i < size; i++)
 		{
 			row += "* ";
 		}
 		row += "*";
-		for (int j = 0; j < size-1; j++)
+		for (int j = 1; j < size; j++)
 		{
-			if (j % 2 == 1) { //Adds first " " for even rows
+			if (j % 2 == 0) { //Adds first " " for even rows
 				System.out.print(" ");
 			}
 			System.out.println(row);
